0ba504fe0d5a48179fa7c5ecf119a6214de18f51
Oliver Magnusson <118819332+Xurasta@users.noreply.github.com>
2024-11-11 11:33:32 +0100

    dfg

--- src/Game.js
+++ src/Game.js
@@ -1,18 +1,32 @@
 import GameObject from "./GameObject"
+import Ball from "./Ball"
+import Input from "./Input"
+import Player from "./Player"
+import Enemy from "./Enemy"
 
 export default class Game {
     constructor(width, height) {
         this.width = width
         this.height = height
+        this.input = new Input(this)
+        this.player = new Player(0, 0, 50, 50, "green", this)
+        this.enemy = new Enemy(500, 200, 100, 100, "red", this)
         console.log("bredden: ", this.width)
-        this.box = new GameObject(40, 100, 200, 200, "purple")
+        //this.box = new GameObject(40, 100, 200, 200, "purple")
+        //this.ball = new Ball(100, 200, 100, 100, "red")
     }
 
     update(deltaTime) {
-        this.box.update(deltaTime)
+        //this.box.update(deltaTime)
+        //this.ball.update(deltaTime)
+        this.player.update(deltaTime)
+        this.enemy.update(deltaTime)
     }
 
     draw(ctx) {
-        this.box.draw(ctx)
+        //this.box.draw(ctx)
+        //this.ball.draw(ctx)
+        this.player.draw(ctx)
+        this.enemy.draw(ctx)
     }
 }
---
+++ src/Input.js
@@ -0,0 +1,25 @@
+export default class Input {
+    constructor(game) {
+        this.game = game
+        this.keys = new Set()
+        this.mouseX = 0
+        this.mouseY = 0
+
+        window.addEventListener("keydown", (event) => {
+            this.keys.add(event.key)
+            console.log(event.key)
+        })
+
+        window.addEventListener("keyup", (event) => {
+            this.keys.delete(event.key)
+        })
+
+        /*window.addEventListener("mousemove", (event) => {
+            this.mouseX = event.movementX
+            this.mouseY = event.movementY
+            console.log("X:" + event.clientX)
+            console.log("Y:" + event.clientY)
+        })
+        */
+    }
+}
