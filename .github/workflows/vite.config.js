9309fdf0fbd146d5e09204c7abf7a435fb0fe0d7
Oliver Magnusson <118819332+Xurasta@users.noreply.github.com>
2025-02-10 11:17:00 +0100

    g

--- .github/workflows/vite.config.js
+++
@@ -1,8 +0,0 @@
-
-<<<<<<< HEAD:.github/workflows/vite.config.js
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/te22-spel',
-})
-=======
->>>>>>> origin/main:.github/vite.config.js
